deaa374d1090f8bd579d264ea9c0231ccc53a02d
PC-22agro <110354767+PC-22agro@users.noreply.github.com>
2022-10-31 17:30:23 +0100

    A profile page

---
+++ my-app/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <img className=" profile_img" src="/sprinkles.png" alt="profilephoto" />
+
+      <h2>PerpetualC</h2>
+
+      <a href="https://twitter.com/perpetualAgwar1" className="twitter">Twitter Link</a>
+
+      <a href="https://hng9.slack.com/perpetualC" className="slack">Slack</a>
+
+
+      <a href="https://training.zuri.team/" className="btn_zuri">Zuri Team</a>
+
+
+      <a href="http://books.zuri.team" className="books">Zuri Books</a>
+
+
+      <a href=" https://books.zuri.team/python-for-beginners?ref_id=<\PerpetualC>" className="book_python">Python Books</a>
+
+
+      <a href="https://background.zuri.team" className="pitch">Get Background Checks for Coders</a>
+
+
+      <a href=" https://books.zuri.team/design-rules" className="book_design">Design Books</a>
+
+    </div>
+  );
+}
+
+export default App;
